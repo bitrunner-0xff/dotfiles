dbf9e5403b41b15f998d17916ea0df96d18e7fee
glen <mrflint46@gmail.com>
2026-02-15 20:31:34 +0100

    Update and fix nvim configs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,64 +2,63 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.guicursor = ""
-vim.opt.termguicolors = true
+local opt = vim.opt
+
+opt.guicursor = ""
+opt.termguicolors = true
 
 -- APPEARANCE -----------------------------------------------------------------
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.wrap = false
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.splitbelow = true
+opt.splitright = true
+opt.wrap = false
 
 -- TAB INDENT -----------------------------------------------------------------
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
 
 -- MISC ----------------------------------------------------------------------
-vim.opt.swapfile = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-vim.opt.backup = false
-vim.opt.writebackup = false
+opt.swapfile = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+opt.backup = false
+opt.writebackup = false
 
--- TODO: check if anything is broken
-vim.opt_global.completeopt = { "menu", "menuone", "noselect" }
-vim.opt.conceallevel = 3 -- Hide * markup for bold and italic
-vim.opt.pumblend = 10 -- Popup blend
-vim.opt.pumheight = 10 -- Maximum number of entries in a popup
+opt.completeopt = { "menu", "menuone", "noselect" }
+opt.conceallevel = 3 -- Hide * markup for bold and italic
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Maximum number of entries in a popup
 -- Highlight matching brace
-vim.opt.showmatch = true
-vim.opt.complete = "kspell" -- TODO: test if it works at all
--- vim.opt.autocomplete = true
-vim.opt.spelllang = { "en" }
-vim.opt.spellsuggest = { "best", 9 }
-vim.opt_global.diffopt:append({ "vertical" })
-vim.opt.wildignorecase = true
-vim.opt.confirm = true
-vim.opt.autowriteall = true
-vim.opt.list = true
-vim.opt.listchars = { tab = [[│ ]], trail = "•", extends = "»", precedes = "«" }
-vim.opt.foldenable = false
+opt.showmatch = true
+opt.complete = "kspell" -- TODO: test if it works at all
+-- opt.autocomplete = true
+opt.spelllang = { "en" }
+opt.spellsuggest = { "best", 9 }
+opt.wildignorecase = true
+opt.confirm = true
+opt.autowriteall = true
+opt.list = true
+opt.listchars = { tab = [[│ ]], trail = "•", extends = "»", precedes = "«" }
+opt.foldenable = false
 
 -- SEARCH ---------------------------------------------------------------------
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+opt.scrolloff = 8
+opt.signcolumn = "yes"
 
-vim.opt.updatetime = 100
-vim.opt.timeoutlen = 500
+opt.updatetime = 100
+opt.timeoutlen = 500
 
-vim.opt.colorcolumn = "80"
-vim.opt.background = "dark"
+opt.colorcolumn = "80"
+opt.background = "dark"
 
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
+opt.clipboard = "unnamedplus" -- use system clipboard
